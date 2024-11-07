cc652ea54b95321abf4b484e7ea1d8ecae4c7461
Josh Espinoza <joshespi@gmail.com>
2024-11-07 13:48:43 -0700

    load child theme styles.css

--- wp-content/themes/caps-2024-child/functions.php
+++ wp-content/themes/caps-2024-child/functions.php
@@ -4,9 +4,9 @@ function my_theme_variables()
 {
     $my_theme_variables = array(
         'logo' => get_stylesheet_directory_uri() . '/assets/img/site-logo.png',
-        'full_school_name' => 'Provo High School',
-        'short_school_name' => 'Provo High',
-        'school_address' => '1199 N. Lakeshore Dr. Provo, Utah 84601',
+        'full_school_name' => 'Provo Center for Advanced Professional Studies (CAPS)',
+        'short_school_name' => 'Provo CAPS',
+        // 'school_address' => '1199 N. Lakeshore Dr. Provo, Utah 84601',
         'google_tag_manager_id' => 'G-FTPJPV04N2',
         'top_sidebar_cal' => '[calendar id="2064"]',
         'bot_sidebar_cal' => '[calendar id="2066"]',
@@ -30,5 +30,7 @@ function sidebar_menu()
 function pcsd_child_theme_enqueue_styles()
 {
     wp_enqueue_style('variables', get_stylesheet_directory_uri() . '/assets/css/variables.css', '', '1.0.0', false);
+    //if custom styles are needed
+    wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css');
 }
 add_action('wp_enqueue_scripts', 'pcsd_child_theme_enqueue_styles', 9999);

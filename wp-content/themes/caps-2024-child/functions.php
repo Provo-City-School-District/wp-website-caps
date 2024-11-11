97b1eec5eb1f5b1a95cb60274169eb5c2fc61fe0
Josh Espinoza <joshespi@gmail.com>
2024-11-11 07:19:37 -0700

    remove remainder phs left overs

--- wp-content/themes/caps-2024-child/functions.php
+++ wp-content/themes/caps-2024-child/functions.php
@@ -8,25 +8,25 @@ function my_theme_variables()
         'short_school_name' => 'Provo CAPS',
         // 'school_address' => '1199 N. Lakeshore Dr. Provo, Utah 84601',
         'google_tag_manager_id' => 'G-FTPJPV04N2',
-        'top_sidebar_cal' => '[calendar id="2064"]',
-        'bot_sidebar_cal' => '[calendar id="2066"]',
+        // 'top_sidebar_cal' => '[calendar id="2064"]',
+        // 'bot_sidebar_cal' => '[calendar id="2066"]',
         'front_page_cal' => '[calendar id="102"]',
         'insta_link' => '#',
         'facebook_link' => '#',
         // 'twitter_link' => '#',
-        'full_calendar_link' => 'https://provohigh.provo.edu/school-calendar/',
+        'full_calendar_link' => 'http://158.91.1.130:8168/calendar/',
         // 'search_icon' => get_template_directory_uri() . '/assets/icons/search-loupe.svg',
-        'blogLink' => 'https://provohigh.provo.edu/category/news/',
+        'blogLink' => 'http://158.91.1.130:8168/category/news/',
     );
     return $my_theme_variables;
 }
-function sidebar_menu()
-{
-    //TODO: add a wp menu here instead of hardcoding
-    echo "<ul>";
-    echo "<li><a href='https://provohigh.provo.edu/school-calendar/a-b-calendar-month-view/'>A/B Calendar Month View</a></li>";
-    echo "</ul>";
-}
+// function sidebar_menu()
+// {
+//     //TODO: add a wp menu here instead of hardcoding
+//     echo "<ul>";
+//     echo "<li><a href='https://provohigh.provo.edu/school-calendar/a-b-calendar-month-view/'>A/B Calendar Month View</a></li>";
+//     echo "</ul>";
+// }
 function pcsd_child_theme_enqueue_styles()
 {
     wp_enqueue_style('variables', get_stylesheet_directory_uri() . '/assets/css/variables.css', '', '1.0.0', false);
